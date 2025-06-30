b5fcce4def3941ee84f66beb9c97dbb58e15ca0b
jukuren48 <info@juku-ren.jp>
2025-06-30 16:18:11 +0900

    Fix englishtrapquestions4th

--- src/components/EnglishTrapQuestions.jsx
+++ src/components/EnglishTrapQuestions.jsx
@@ -108,20 +108,6 @@ export default function EnglishTrapQuestions() {
     return <div className="p-8 text-lg">読み込み中です...</div>;
   }
 
-  if (showQuestions && filteredQuestions.length === 0) {
-    return (
-      <div className="max-w-4xl mx-auto p-4 text-center">
-        <p className="text-lg">問題が選択されていません。</p>
-        <button
-          onClick={handleRetry}
-          className="bg-blue-500 text-white px-6 py-2 rounded mt-4"
-        >
-          戻る
-        </button>
-      </div>
-    );
-  }
-
   return (
     <div className="max-w-4xl mx-auto p-4">
       {showQuestions && !showResult && (
