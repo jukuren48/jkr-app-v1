18fbff86476128f6cc17dde99d6ff17cda2ac821
jukuren48 <info@juku-ren.jp>
2025-11-25 21:36:59 +0900

    loginfix

--- pages/login.jsx
+++ pages/login.jsx
@@ -33,8 +33,8 @@ export default function Login() {
   const googleLogin = async () => {
     const redirectUrl =
       process.env.NODE_ENV === "development"
-        ? "http://localhost:3000/"
-        : "https://jkr-app-v1.vercel.app/";
+        ? "http://localhost:3000/auth/callback"
+        : "https://jkr-app-v1.vercel.app/auth/callback";
 
     await supabase.auth.signInWithOAuth({
       provider: "google",
