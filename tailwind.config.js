c303dafa23c0d318b2779d82e96804fc445067bb
jukuren48 <info@juku-ren.jp>
2025-11-26 12:21:40 +0900

    fixlogin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}", // ★ これを追加！
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
   safelist: [
     // --- 既存 ---
     "bg-orange-400",
